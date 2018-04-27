a68f55d76e90e4f86b9facfecbe61aeafe72b8e4
hamirshekhawat <devhamir@gmail.com>
2018-04-27 23:48:37 +0530

    Update Sokoban.py

--- gym-sokoban/Sokoban.py
+++ gym-sokoban/Sokoban.py
@@ -5,9 +5,9 @@ import numpy as np
 
 class SokobanEnv:
     def __init__(self):
-        self.env = gym.make('Sokoban-v0')
+        self.env = gym.make('TinyWorld-Sokoban-small-v0')
         self.action_space      = self.env.action_space
-        self.observation_space = spaces.Box(low=0, high=255, shape=(3, 160, 160), dtype=np.uint8)
+        self.observation_space = spaces.Box(low=0, high=255, shape=(3, 80, 80), dtype=np.uint8)
 
     def step(self, action):
         observation, reward, self.done, _ = self.env.step(action)
